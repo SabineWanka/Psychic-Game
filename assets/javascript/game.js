ad7164495457a5dfebfbf858b278d49c44934238
SabineWanka <sabinewanka@yahoo.de>
2019-10-01 18:22:41 +0200

    quick debugging

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
-var options = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
 var guesses = 0;
 
-document.onkeyup = function () {
+document.onkeyup = function (event) {
     var userGuess = String.fromCharCode(event.keyCode).
         toLowerCase();
     console.log(userGuess)
 
-    var computerGuess = options[Math.floor(Math.random() * options.lenght)];
+    var computerGuess = options[Math.floor(Math.random() * options.length)];
 
     console.log(computerGuess);
 
