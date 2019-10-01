22b55cdd0886adb00e54bec3914c5f186c263861
SabineWanka <sabinewanka@yahoo.de>
2019-10-01 18:15:06 +0200

    quick debugging

---
+++ assets/javascript/game.js
@@ -0,0 +1,37 @@
+var options = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+var wins = 0;
+var losses = 0;
+var guesses = 0;
+
+document.onkeyup = function () {
+    var userGuess = String.fromCharCode(event.keyCode).
+        toLowerCase();
+    console.log(userGuess)
+
+    var computerGuess = options[Math.floor(Math.random() * options.lenght)];
+
+    console.log(computerGuess);
+
+
+    if (userGuess === computerGuess) {
+        wins++
+        alert("wins" + wins)
+
+    }
+    else {
+        losses++
+        alert("losses" + losses)
+    }
+
+
+
+
+    var html = "<p>Guess what letter I'm thinking of</p>" +
+        "<p>wins: " + wins + "</p>" +
+        "<p>losses: " + losses + "</p>" +
+        "<p> Guesses left: " - guesses - "</p>" +
+        "<p> Your Guesses so far: " + guesses + "</p>"
+
+
+    document.querySelector("#game").innerHTML = html;
+}
